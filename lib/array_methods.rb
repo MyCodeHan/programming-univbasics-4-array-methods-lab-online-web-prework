7e5c55d82226f62947ce0f87eb497c247f5a8647
Hannah Harrington <hannahharrington@outlook.com>
2019-08-07 01:47:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
-end
+  array.sort
+  end
 
 def using_reverse(array)
+# using_include (array = "a,b,c,d")
+# array.reverse #=> ["c", "b", "a"
+# end
 
-end
+# def using_first (array)
+# (array = "a,b,c,d")
+# arry.first #=> 1
+# end
 
-def using_first(array)
+# def using_last(array)
+# (array = "a,b,c,d")
+# array.last #=> 4
+# end
 
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
-
-end
+# def using_size(array)
+# (array = "a,b,c,d")
+# Array[ 1, 'a', /^A/ ]    
+# # => [1, "a", /^A/]
+# end
